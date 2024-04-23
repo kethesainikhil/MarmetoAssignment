ab3fe4685d8cd778458f9cbbb50235663663aad7
kethesainikhil <nikhilkethe007@gmail.com>
2024-04-23 18:47:35 +0530

    some changes made

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Select all elements with the class 'tab'
-
 const clearProductCards = () => {
     const container = document.querySelector('.product-container');
     container.innerHTML = '';
 };
+
 const fetchAndDisplayProducts = async (category) => {
     try {
-        clearProductCards()
+        clearProductCards();
         const response = await fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json');
         const data = await response.json();
 
@@ -19,6 +18,7 @@ const fetchAndDisplayProducts = async (category) => {
         console.error('Error fetching products:', error);
     }
 };
+
 const createProductCard = (product) => {
     const container = document.querySelector('.product-container');
 
@@ -33,21 +33,15 @@ const createProductCard = (product) => {
     img.style.borderRadius = '5px';
     card.appendChild(img);
 
-
     // Add badge text
     const badgeText = document.createElement('div');
     badgeText.classList.add('badge');
     badgeText.textContent = product.badge_text || null;
     card.appendChild(badgeText);
-    console.log(product.badge_text)
-    if((product.badge_text == null)) {
-        
-        console.log("hey one iem")
+
+    if (product.badge_text == null) {
         badgeText.style.display = 'none';
-    }
-    else{
-        console.log("hey two iem")
-        
+    } else {
         badgeText.style.padding = '5px 2px';
     }
 
@@ -60,19 +54,18 @@ const createProductCard = (product) => {
     titleVendor.classList.add('title-vendor');
     const productTitle = document.createElement('h3');
     const productVendor = document.createElement('p');
+
     if (product.title.length > 10) {
-        product.title = product.title.substring(0, 10); // Get the first 10 characters
-        // Add an ellipsis (...) to indicate that the text has been truncated
+        product.title = product.title.substring(0, 10);
     }
 
     productTitle.textContent = `${product.title}`;
-
     productVendor.textContent = `${product.vendor}`;
     productVendor.style.fontWeight = "200";
     productVendor.style.fontSize = "15px";
-    productVendor.classList.add('vendor')
+    productVendor.classList.add('vendor');
     titleVendor.appendChild(productTitle);
-    titleVendor.appendChild(productVendor)
+    titleVendor.appendChild(productVendor);
 
     details.appendChild(titleVendor);
 
@@ -80,80 +73,67 @@ const createProductCard = (product) => {
     const price = document.createElement('div');
     price.classList.add('price');
     price.textContent = `Price: ₹${product.price}`;
-    // details.appendChild(price);
 
     if (product.compare_at_price) {
-    const compareAtPrice = document.createElement('div');
-    compareAtPrice.classList.add('compare-price');
-    compareAtPrice.id = 'compare-at-price';
-    compareAtPrice.textContent = `₹${product.compare_at_price}`;
-    // compareAtPrice.style.textDecoration = 'line-through'; // Apply line-through to compareAtPrice
+        const compareAtPrice = document.createElement('div');
+        compareAtPrice.classList.add('compare-price');
+        compareAtPrice.id = 'compare-at-price';
+        compareAtPrice.textContent = `₹${product.compare_at_price}`;
 
-    // Calculate discount percentage
-    const discount = Math.round(((product.compare_at_price - product.price) / product.compare_at_price) * 100);
-    const discountText = document.createElement('span'); // Use span instead of div
-    discountText.classList.add('discount');
-    discountText.id = 'discount-id';
-    discountText.textContent = ` (${discount}% off)`;
-    if (discount > 0) {
-        discountText.style.color = 'red';
-        discountText.style.textDecoration = 'none'; // Remove text decoration from discountText
-    } else {
-        discountText.style.textDecoration = 'none'; // Ensure text decoration is removed even if discount is 0
+        // Calculate discount percentage
+        const discount = Math.round(((product.compare_at_price - product.price) / product.compare_at_price) * 100);
+        const discountText = document.createElement('span');
+        discountText.classList.add('discount');
+        discountText.id = 'discount-id';
+        discountText.textContent = ` (${discount}% off)`;
+        
+        if (discount > 0) {
+            discountText.style.color = 'red';
+            discountText.style.textDecoration = 'none';
+        } else {
+            discountText.style.textDecoration = 'none';
+        }
+
+        const lastColumn = document.createElement('div');
+        lastColumn.classList.add('last-column');
+        lastColumn.appendChild(price);
+        lastColumn.append(compareAtPrice); 
+        lastColumn.append(discountText);
+        
+        details.appendChild(lastColumn);
     }
-    const lastColumn = document.createElement('div');
-    lastColumn.classList.add('last-column');
-    lastColumn.appendChild(price);
-    lastColumn.append(compareAtPrice); 
-    lastColumn.append(discountText);
-    
-    
-
-    // Append discount text to compareAtPrice
-    
-    details.appendChild(lastColumn)// Append compareAtPrice to details
-}
-
-    
-   
 
     card.appendChild(details);
-     // Append details to card
-     const button = document.createElement('button');
-     button.classList.add('button1');
-     button.textContent = 'Add to cart';
-     card.appendChild(button);
+    
+    // Append details to card
+    const button = document.createElement('button');
+    button.classList.add('button1');
+    button.textContent = 'Add to cart';
+    card.appendChild(button);
 
-    container.appendChild(card); // Append card to container
+    container.appendChild(card);
 };
 
-
-
-
 const tabs = document.querySelectorAll('.tab');
-console.log(tabs)
+
 // Iterate over each tab and add a click event listener
 tabs.forEach(tab => {
     tab.addEventListener('click', (e) => {
-        // Your event handling code goes here
         const category = tab.innerText.trim(); 
-        fetchAndDisplayProducts(category)
-               // Remove black background and reset opacity for all tabs
-            tabs.forEach(otherTab => {
-            otherTab.style.backgroundColor = ''; // Reset background color
+        fetchAndDisplayProducts(category);
+        
+        // Remove black background and reset opacity for all tabs
+        tabs.forEach(otherTab => {
+            otherTab.style.backgroundColor = '';
             const otherImg = otherTab.querySelector('.icon');
-            otherImg.style.opacity = ''; // Reset opacity
+            otherImg.style.opacity = '';
         });
-    
-            // Set the background color of the clicked tab to black
-            tab.style.backgroundColor = 'black';
-    
-            // Get the image element inside the clicked tab
-            const img = tab.querySelector('.icon');
-    
-            // Set the opacity of the image to 100%
-            img.style.opacity = '1';
-
+        
+        // Set the background color of the clicked tab to black
+        tab.style.backgroundColor = 'black';
+        
+        // Get the image element inside the clicked tab
+        const img = tab.querySelector('.icon');
+        img.style.opacity = '1';
     });
 });
-
